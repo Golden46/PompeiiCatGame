6698ebe983061d851bd9f094f9b1dcee9f6bb29f
Corey Shepherd <coreyshepherd499@gmail.com>
2026-04-17 00:23:20 +0100

    .

--- Assets/Scripts/Quest/Object/ObjectInteract.cs
+++ Assets/Scripts/Quest/Object/ObjectInteract.cs
@@ -4,17 +4,25 @@ public class ObjectInteract : MonoBehaviour
 {
     [SerializeField] private string itemID;
     [SerializeField] private Transform itemPopup;
+    [SerializeField] private bool destroyObject;
+    [SerializeField] private string animationTrigger;
+    
+    private Animator _animator;
     private QuestManager _questManager;
     
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         _questManager = QuestManager.Instance;
     }
 
     public void Pickup()
     {
         var collectItem = _questManager.CheckObjective(itemID); // Check to either complete an objective or pick up objective item
-        if (collectItem) Destroy(gameObject); // If either happen then delete the real world object
+        if (!collectItem) return;
+        
+        if (destroyObject) Destroy(gameObject); // If either happen then delete the real world object
+        if (_animator != null) _animator.SetTrigger(animationTrigger);
     }
     
     private void OnTriggerStay(Collider other)
